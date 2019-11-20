7c585338bed62a2e7e3bceeadb57d7113eb7afb2
mandinga25 <gusta.king.25@gmail.com>
2019-11-20 20:45:55 -0300

    Segunda Version

--- src/saboresgustavo/SaboresGustavo.java
+++ src/saboresgustavo/SaboresGustavo.java
@@ -7,7 +7,8 @@ public class SaboresGustavo {
     
     public static void main(String[] args) {
         String[] sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo"};
-        for(int i =0; i<6;i++){
+        System.out.println("Existen 10 tipos de sabores:");
+        for(int i =0; i<8;i++){
             System.out.println("Sabor: "+ sabores[i]);
         }
     }
