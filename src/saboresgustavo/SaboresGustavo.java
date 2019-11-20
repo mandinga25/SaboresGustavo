fa57e0a5b7f15069eab2dcad3abf726b22a5cd3e
mandinga25 <gusta.king.25@gmail.com>
2019-11-20 20:15:48 -0300

    Primera version

---
+++ src/saboresgustavo/SaboresGustavo.java
@@ -0,0 +1,15 @@
+
+package saboresgustavo;
+
+
+public class SaboresGustavo {
+
+    
+    public static void main(String[] args) {
+        String[] sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo"};
+        for(int i =0; i<6;i++){
+            System.out.println("Sabor: "+ sabores[i]);
+        }
+    }
+    
+}
