1ae161ee0d48f912a6cc085318d3f2ccbbf692ef
Your-Full-Name <your-email-address>
2019-11-20 20:34:24 -0300

    Cambio de sabores.

--- src/saboresgustavo/SaboresGustavo.java
+++ src/saboresgustavo/SaboresGustavo.java
@@ -6,7 +6,7 @@ public class SaboresGustavo {
 
     
     public static void main(String[] args) {
-        String[] sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo"};
+        String[] sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo", "Menta", "Fuerte"};
         for(int i =0; i<6;i++){
             System.out.println("Sabor: "+ sabores[i]);
         }
